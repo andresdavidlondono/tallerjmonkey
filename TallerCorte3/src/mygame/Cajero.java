fcaf823eab3aa44b305c5d37dd0682575cd183d3
andresdavidlondono <30504999+andresdavidlondono@users.noreply.github.com>
2018-04-21 20:04:59 -0500

    Add files via upload

---
+++ TallerCorte3/src/mygame/Cajero.java
@@ -0,0 +1,26 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mygame;
+
+/**
+ *
+ * 
+ */
+public class Cajero {
+    
+    private int TiempoInicio;
+    private int TiempoFin;
+    private int IDCajero;
+    private Cliente CLienteActual; //falta importar la libreria, de ahí el error
+    private int TiempoTotalLibre;
+    private int TiempoTotalOcupado;
+    private int TotalClientes;
+
+    
+    
+    
+    
+    
+}
